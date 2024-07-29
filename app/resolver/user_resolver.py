e9e8e51500d8d7d958c62a8a41db77f67eae67fd
ilhan <ilhanabdalle@gmail.com>
2024-07-29 14:36:31 +0100

    Get all users updated to include list of interests & GET interest resolver & query added

--- app/resolver/user_resolver.py
+++ app/resolver/user_resolver.py
@@ -2,11 +2,12 @@ from sqlalchemy import Result, Sequence, Tuple, insert
 from sqlalchemy.orm import selectinload
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from app.models.interest_model import Interest
+from app.models.interest_model import Interest as InterestModel
 from app.models.user_model import User as UserModel
 
 async def get_all_users(session: AsyncSession):
-    result: Result[Tuple] = await session.execute(select(UserModel))
+    stmt = (select(UserModel).options(selectinload(UserModel.interests)))
+    result: Result[Tuple[UserModel]] = await session.execute(stmt)
     users: Sequence[UserModel] = result.scalars().all()
     return users
 
@@ -20,3 +21,7 @@ async def get_user(session: AsyncSession, user_id: int):
     user: UserModel = result.scalars().first()
     return user
 
+async def get_all_interests(session: AsyncSession):
+    result: Result[Tuple[InterestModel]]= await session.execute(select(InterestModel))
+    interests: Sequence[InterestModel] = result.scalars().all()
+    return interests
--- app/schemas/query_schema.py
+++ app/schemas/query_schema.py
@@ -1,7 +1,7 @@
 import strawberry
-from app.resolver.user_resolver import get_all_users, get_user
+from app.resolver.user_resolver import get_all_interests, get_all_users, get_user
 from app.db.session import get_session
-from app.schemas.types_schema import User
+from app.schemas.types_schema import User, Interest
 
 @strawberry.type
 class Query:
@@ -17,3 +17,9 @@ class Query:
         async with get_session() as session:
             user = await get_user(session, user_id)
             return user
+
+    @strawberry.field
+    async def interests(self) -> list[Interest]:
+        async with get_session() as session:
+            interests = await get_all_interests(session)
+            return interests
