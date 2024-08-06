8481ce877de3d606a9dbd75729279d5ffe5413f5
ilhan <ilhanabdalle@gmail.com>
2024-08-06 09:27:43 +0100

    Adds verify password & refactors hash passworf function

--- app/utils/password_hasher.py
+++ app/utils/password_hasher.py
@@ -2,5 +2,11 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def hash_password(password:str):
-    return pwd_context.hash(password)
+class Hasher():
+
+    def hash_password(password: str) -> str:
+        return pwd_context.hash(password)
+
+    def verify_password(form_password: str, hashed_password: str) -> bool:
+        return pwd_context.verify(form_password, hashed_password)
+
