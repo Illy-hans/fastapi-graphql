80fc068f4953b3fe0ffff161ef76c38df3b2fc5f
ilhan <ilhanabdalle@gmail.com>
2024-08-09 17:11:53 +0100

    Adds user authentication- create + decode token

--- app/tests/user/test_user.py
+++ app/tests/user/test_user.py
@@ -47,3 +47,57 @@ async def test_email_in_use():
     assert result.errors == None
     assert result.data["createUser"] == 'Email address is in use'
 
+
+# token variable to be re-used in decode token
+auth_token = None 
+
+@pytest.mark.asyncio
+async def test_create_token_for_user():
+    create_token_mutation = """
+            mutation MyMutation {
+                login(email: "testuser@example.com", password: "testpassword") {
+                accessToken
+                userName
+                }
+            }
+"""
+
+    result = await schema.execute(create_token_mutation)
+    print(result)
+
+    assert result.errors == None
+    token = result.data['login']["accessToken"]
+    assert token != ""
+
+    global auth_token
+    auth_token = token
+    print(auth_token)
+
+
+# user_id variable to be used in subsequent tests
+user_id = None
+
+@pytest.mark.asyncio 
+async def test_decode_token():
+    decode_token_mutation_return_user = """
+            mutation DecodeToken($token: String!) {
+                decodeUserFromToken(token: $token) {
+                    name
+                    email
+                    id
+                }
+            }
+"""
+
+    result = await schema.execute(decode_token_mutation_return_user, variable_values={"token": auth_token})
+    
+    assert result.errors is None
+    assert result.data['decodeUserFromToken']['name'] == "Test User"
+    assert result.data['decodeUserFromToken']['email'] == "testuser@example.com"
+    assert result.data['decodeUserFromToken']['id'] != None
+
+    global user_id 
+    user_id = result.data['decodeUserFromToken']['id']
+
+
+
